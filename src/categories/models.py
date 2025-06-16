75f8740d9a13fc98ab4883cb44b82e2abe2d58e3
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-06-16 17:44:28 -0300

    Models added: brands, categories, products, suppliers

---
+++ src/categories/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+    
+    def __str__(self):
+        return self.name
---
+++ src/suppliers/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Supplier(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Fornecedor'
+        verbose_name_plural = 'Fornecedores'
+    
+    def __str__(self):
+        return self.name
